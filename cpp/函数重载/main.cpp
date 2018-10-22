174cc803070ff1aa7d723c13440dfe4a6443946a
MonologueX <3280795699@qq.com>
2018-10-22 12:49:49 +0800

    c++

--- cpp/函数重载/main.cpp
+++ cpp/函数重载/main.cpp
@@ -4,7 +4,7 @@
 //  编译环境:Linux
 //  作者相关:心文花雨
 
-////////////////////////////////////////////////
+/////////////////////////////// 
 // 头文件
 #include <iostream>
 using namespace std;
@@ -27,21 +27,37 @@ using namespace std;
 //}
 
 
-void Swap(int* left, int* right)
-{
-  int temp = *left;
-  *left = *right;
-  *right = temp;
-}
-
+//void Swap(int* left, int* right)
+//{
+//  int temp = *left;
+//  *left = *right;
+//  *right = temp;
+//}
+//
+//int main()
+//{
+//  int left = 10;
+//  int right = 20;
+//  
+//  Swap(&left, &right);
+//  cout << left << endl;
+//  cout << right << endl;
+//
+//  return 0;
+//}
+//int main()
+//{
+//  const int a = 10;
+//  const int& ra = a;
+//  const int& b = a;
+//
+//  double d = 3.14;
+//  const int& rd = d; 
+//
+//  return 0;
+//}
 int main()
 {
-  int left = 10;
-  int right = 20;
   
-  Swap(&left, &right);
-  cout << left << endl;
-  cout << right << endl;
-
   return 0;
 }
