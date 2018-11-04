dcf1a2bb220c44e9d1ba00428576eb7ee5cbb87b
MonologueX <3280795699@qq.com>
2018-11-04 08:50:16 +0800

    mysql_5_test

---
+++ mysql/mysql5.sql
@@ -0,0 +1,9 @@
+#文件名称:mysql5
+#文件描述:练习
+#编译环境:Linux
+#作者相关:心文花雨 
+#create database mysql5test;
+#use mysql5test;
+#create table tt1(id int, name varchar(10), password varchar(15));
+#insert into  tt1 values (1,"caihao","vdvsj");
+#select *from tt1;
