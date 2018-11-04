557c9997df9c34233855f9d92e713de46fe47ef6
MonologueX <3280795699@qq.com>
2018-11-04 10:31:05 +0800

    练习

--- mysql/mysql5.sql
+++ mysql/mysql5.sql
@@ -7,3 +7,16 @@
 #create table tt1(id int, name varchar(10), password varchar(15));
 #insert into  tt1 values (1,"caihao","vdvsj");
 #select *from tt1;
+#create table tt3();
+#desc tt3;
+#select * from tt3;
+#create table stu (id int, name varchar(30)); -- 学生表
+#insert into stu values(1,'jack'),(2,'tom'),(3,'kity'),(4,'nono');
+#create table exam (id int, grade int); -- 成绩表
+#insert into exam values(1, 56),(2,76),(11, 8);
+#select * from stu left join exam on stu.id=exam.id;
+#select * from stu right join exam on stu.id=exam.id; 
+#列出部门名称和这些部门的员工信息，同时列出没有员工的部门
+#select dept.dname emp where
+#    select from dept left join emp on dept.deptno=emp.deptno;
+
