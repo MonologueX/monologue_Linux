2c434c96bc84316fad7e697a8dbb0df92893f8fa
MonologueX <3280795699@qq.com>
2018-11-05 22:45:11 +0800

    cpp1

--- cpp/练习/main.cpp
+++ cpp/练习/main.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////// 
 // 文件名称:C++
-// 文件描述:无
+// 文件描述:2018-11-5
 // 编译环境:Linux
 // 作者相关:心文花雨 
 
@@ -13,11 +13,29 @@ using namespace std;
 ///////////////////////////////
 // 函数
 
+#include <iostream>
+using namespace std;
+int a = 10;
+
+namespace N1
+{
+    int a = 1;
+    int Add(int left, int right)
+    {
+        return left + right;
+    }
+}
+
+int main()
+{
+    
+    return 0;
+}
 
 ///////////////////////////////
 // 主函数
 int main()
 {
-    printf("");
+    cout << "hello word" << endl;
     return 0;
 }
