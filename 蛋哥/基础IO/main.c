78e7a931f9e9fa68ed4d5a96da63518669bb3c34
MonologueX <3280795699@qq.com>
2018-11-10 15:07:27 +0800

    补充

--- 蛋哥/基础IO/main.c
+++ 蛋哥/基础IO/main.c
@@ -1,6 +1,6 @@
 /////////////////////////////// 
-// 文件名称:
-// 文件描述:
+// 文件名称:main.c
+// 文件描述:无
 // 编译环境:Linux
 // 作者相关:心文花雨 
 #include <stdio.h>
@@ -10,69 +10,84 @@
 #include <fcntl.h>
 #include <string.h>
 
-///////////////////////////////
-// 写
+#include <stdio.h>
 
+int main()
+{
+    const char* msg1 = "hello printf\n";
+    const char* msg2 = "hello fwrinte\n";
+    const char* msg3 = "hello write\n";
+
+    printf(msg1);
+    fwrite(msg2, 1, strlen(msg2), stdout);
+    write(1, msg3, strlen(msg3));
+    //fork();
+    return 0;
+}
+
+/////////////////////////////////
+//// 写
+//
+////int main()
+////{
+////    int fd = open("file", O_WRONLY|O_CREAT, 0644);
+////    if (fd < 0)
+////    {
+////        perror("open");
+////        return 1;
+////    }
+////    const char* msg = "hello word\n";
+////    int count = 5;
+////        
+////    while (count--)
+////    {
+////        write(fd, msg, strlen(msg));
+////    }
+////
+////    close(fd);
+////    return 0;
+////}
+//
+/////////////////////////////////
+//// 读
+//
 //int main()
 //{
-//    int fd = open("file", O_WRONLY|O_CREAT, 0644);
+//    int fd = open("file", O_WRONLY, 0644);
 //    if (fd < 0)
 //    {
 //        perror("open");
 //        return 1;
 //    }
-//    const char* msg = "hello word\n";
-//    int count = 5;
-//        
+//    
+//    int count = 10;
 //    while (count--)
 //    {
-//        write(fd, msg, strlen(msg));
+//        printf("hello word\n");
+//        fflush(stdout);
 //    }
-//
 //    close(fd);
+//  //  int fd1 = open("file", O_RDONLY, 0644);
+//  //  int fd2 = open("file", O_RDONLY, 0644);
+//  //  int fd3 = open("file", O_RDONLY, 0644);
+//  //  printf("fd:%d\n", fd);
+//  //  printf("fd:%d\n", fd1);
+//  //  printf("fd:%d\n", fd2);
+//  //  printf("fd:%d\n", fd3);
+//////    const char* msg = "hello word\n";
+////    int size = strlen(msg);
+////    char buf[64];
+////    int count = 5;
+////    while (count--)
+////    {
+////        size_t s = read(fd, buf, size);
+////        if (s > 0)
+////        {
+////            buf[s] = 0;
+////        }
+////        printf("%s", buf);
+////    }
+////
+////    close(fd);
 //    return 0;
 //}
-
-///////////////////////////////
-// 读
-
-int main()
-{
-    int fd = open("file", O_WRONLY, 0644);
-    if (fd < 0)
-    {
-        perror("open");
-        return 1;
-    }
-    
-    int count = 10;
-    while (count--)
-    {
-        printf("hello word\n");
-        fflush(stdout);
-    }
-    close(fd);
-  //  int fd1 = open("file", O_RDONLY, 0644);
-  //  int fd2 = open("file", O_RDONLY, 0644);
-  //  int fd3 = open("file", O_RDONLY, 0644);
-  //  printf("fd:%d\n", fd);
-  //  printf("fd:%d\n", fd1);
-  //  printf("fd:%d\n", fd2);
-  //  printf("fd:%d\n", fd3);
-////    const char* msg = "hello word\n";
-//    int size = strlen(msg);
-//    char buf[64];
-//    int count = 5;
-//    while (count--)
-//    {
-//        size_t s = read(fd, buf, size);
-//        if (s > 0)
-//        {
-//            buf[s] = 0;
-//        }
-//        printf("%s", buf);
-//    }
-//
-//    close(fd);
-    return 0;
-}
