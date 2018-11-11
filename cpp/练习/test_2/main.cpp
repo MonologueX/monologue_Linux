525c241e46e117b7128f4a004b176f0e5c51c9af
MonologueX <3280795699@qq.com>
2018-11-11 15:50:02 +0800

    auto nullptr

--- cpp/练习/test_2/main.cpp
+++ cpp/练习/test_2/main.cpp
@@ -47,15 +47,90 @@ int Add(int a, int b)
 ///////////////////////////////
 // const 修饰常量
 // & 具有宏的属性 替换
+
+//int main()
+//{
+//    const int a = 10;
+//    int *pa = (int*)&a;
+//
+//    *pa = 100;
+//
+//    cout << a << endl;
+//    cout << *pa << endl;
+// 
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 10;
+//    int b = 20;
+//    auto c = 'c';
+//    auto d = 1.5;
+//    auto ret = fun();
+//    cout << typeid(a).name() << endl;
+//    cout << typeid(b).name() << endl;
+//    cout << typeid(c).name() << endl;
+//    cout << typeid(d).name() << endl;
+//    // auto 要初始化
+//    //auto e;
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 10;
+//    auto pa1 = &a;
+//    *pa1 = 20;
+//
+//    auto *pa2 = &a;
+//    *pa2 = 30;
+//
+//    auto &ra = a;
+//    ra = 40;
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//    int array[] = {1, 2, 3, 4, 5, 6, 7};
+//    for (auto &e : array)
+//    {
+//        e *= 2;
+//    }
+//    for (auto &e : array)
+//    {
+//        cout << e << " " << endl;
+//    }
+//    cout << endl;
+//    return 0;
+//}
+
+
+//int main()
+//{
+//   // 错误
+//   // int *p;
+//   // for (auto &e : p)
+//   // {
+//   //     cout << p <<endl;
+//   // return 0;
+//}
+
+void Testfunc(int a)
+{
+    cout << "void Testfunc(int a)" << endl;
+}
+
+void Testfunc(int *a)
+{
+    cout << "void Testfunc(int *a)" << endl;
+}
+
 int main()
 {
-    const int a = 10;
-    int *pa = (int*)&a;
-
-    *pa = 100;
-
-    cout << a << endl;
-    cout << *pa << endl;
- 
+    Testfunc(0);
+    Testfunc(nullptr);
     return 0;
 }
