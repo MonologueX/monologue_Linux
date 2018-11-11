c15b8fc830c75f123c38093ae134ec1a6b0ccd92
MonologueX <3280795699@qq.com>
2018-11-11 14:42:24 +0800

    练习

---
+++ cpp/练习/test_2/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * 文件名称:main.c
+ * 文件描述:练习
+ * 编译环境:Linux 
+ * 作者相关:心文花雨 
+*/ 
+
+#include <iostream>
+using namespace std;
+
+#define Max(a, b) ((a)>(b)?(a):(b))
+
+//int main()
+//{
+//    int a1 = 10;
+//    int a2 = 20;
+//
+//    cout << Max(a1, ++a2) << endl;
+//
+//    return 0;
+//}
+
