e204cbaefc70c1213464e6996567d3deaee6a064
MonologueX <3280795699@qq.com>
2018-11-12 12:15:58 +0800

    类和对象补充

--- cpp/练习/test_3/main.cpp
+++ cpp/练习/test_3/main.cpp
@@ -9,36 +9,42 @@
 #include <string.h>
 using namespace std;
 
-class Student 
-{
-public:
-    char _name[20];
-    char _gender[3];
-    int _age;
-    
-    void Set(char *name, char *gender, int age)
-    {
-        strcpy(_name, name);
-        strcpy(_gender, gender);
-        _age = age;
-    }
-
-    void Print()
-    {
-        cout << _name << " " << _gender << " " << _age << endl;
-    }
-};
+//class Student 
+//{
+//public:
+//    char m_name[20];
+//    char m_gender[3];
+//    int m_age;
+//    
+//    void Set(char *name, char *gender, int age)
+//    {
+//        strcpy(m_name, name);
+//        strcpy(m_gender, gender);
+//        m_age = age;
+//    }
+//
+//    void Print()
+//    {
+//        cout << m_name << " " << m_gender << " " << m_age << endl;
+//    }
+//};
+//
+//int main()
+//{
+//    Student s1,s2,s3;
+//
+//    s1.Set("张三", "男", 15);
+//    s2.Set("李四", "男", 19);
+//    s3.Set("小花", "女", 18);
+//
+//    s1.Print();
+//    s2.Print();
+//    s3.Print();
+//    return 0;
+//}
 
 int main()
 {
-    Student s1,s2,s3;
-
-    s1.Set("张三", "男", 15);
-    s2.Set("李四", "男", 19);
-    s3.Set("小花", "女", 18);
-
-    s1.Print();
-    s2.Print();
-    s3.Print();
+    
     return 0;
-} 
+}
