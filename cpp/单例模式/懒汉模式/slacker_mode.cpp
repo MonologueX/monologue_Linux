1dd6cacf5749d23c39ce4f7e76061b461fcc7d5b
MonologueX <3280795699@qq.com>
2019-01-25 21:09:00 +0800

    懒汉模式

---
+++ cpp/单例模式/懒汉模式/slacker_mode.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Singleton
+{
+    public:
+        static Singleton* GetInstrance()
+        {
+            return &m_ps;
+        }
+    private:
+        Singleton(Singleton &s)
+        {}
+        static Singleton m_ps;
+};
+
+Singleton Singleton::m_ps;
+
+int main()
+{
+
+    return 0;
+}
